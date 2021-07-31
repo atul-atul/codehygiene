7f439268a6d0ffcdec8de79a965ca406162e3dd3
atul <260180@gmail.com>
2021-07-31 17:47:26 +0530

    Removed unused imports

--- src/test/java/codehygiene/CodeHygienePluginTest.java
+++ src/test/java/codehygiene/CodeHygienePluginTest.java
@@ -3,6 +3,12 @@ package codehygiene;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.gradle.api.Project;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class CodeHygienePluginTest {
